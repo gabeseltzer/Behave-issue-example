3c0b5d1ec1710d24df96f4f968322c99e7af9fe7
Gabriel Seltzer <gabriel.seltzer@formlabs.com>
2025-04-11 17:17:51 -0400

    fix logging again

--- features/environment.py
+++ features/environment.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
-import logging
 
 
 from behave.model import Tag
 from behave.runner import Context
 
-logger = logging.getLogger("environment")
-
 
 def before_tag(context: Context, tag: Tag) -> None:
-  logger.error(f"Tag is: {tag}")
+  print(f"THE TAG IS: {tag}")
